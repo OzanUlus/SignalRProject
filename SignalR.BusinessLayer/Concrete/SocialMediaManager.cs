a9285b2224d5bbb3b650714f2b5400b9549c96cf
OzanUlus <ilkerozanulus@gmail.com>
2024-05-13 20:49:02 +0300

    bll done

---
+++ SignalR.BusinessLayer/Concrete/SocialMediaManager.cs
@@ -0,0 +1,46 @@
+﻿using SignalR.BusinessLayer.Abstract;
+using SignalR.DataAccessLayer.Abstract;
+using SignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.BusinessLayer.Concrete
+{
+    public class SocialMediaManager : ISocialMediaService
+    {
+        private readonly ISocialMediaDal _socailMediaDal;
+
+        public SocialMediaManager(ISocialMediaDal socailMediaDal)
+        {
+            _socailMediaDal = socailMediaDal;
+        }
+
+        public void TAdd(SocialMedia entity)
+        {
+            _socailMediaDal.Add(entity);
+        }
+
+        public void TDelete(SocialMedia entity)
+        {
+            _socailMediaDal.Delete(entity);
+        }
+
+        public SocialMedia TGetById(int id)
+        {
+          return _socailMediaDal.GetById(id);
+        }
+
+        public List<SocialMedia> TGetListAll()
+        {
+            return _socailMediaDal.GetListAll();
+        }
+
+        public void TUpdate(SocialMedia entity)
+        {
+            _socailMediaDal.Update(entity);
+        }
+    }
+}
---
+++ SignalR.BusinessLayer/Concrete/TestimoniaManager.cs
@@ -0,0 +1,46 @@
+﻿using SignalR.BusinessLayer.Abstract;
+using SignalR.DataAccessLayer.Abstract;
+using SignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.BusinessLayer.Concrete
+{
+    public class TestimoniaManager : ITestimonialService
+    {
+        private readonly ITestimonial _testimonial;
+
+        public TestimoniaManager(ITestimonial testimonial)
+        {
+            _testimonial = testimonial;
+        }
+
+        public void TAdd(Testimonial entity)
+        {
+            _testimonial.Add(entity);
+        }
+
+        public void TDelete(Testimonial entity)
+        {
+            _testimonial.Delete(entity);
+        }
+
+        public Testimonial TGetById(int id)
+        {
+            return _testimonial.GetById(id);
+        }
+
+        public List<Testimonial> TGetListAll()
+        {
+            return _testimonial.GetListAll();
+        }
+
+        public void TUpdate(Testimonial entity)
+        {
+            _testimonial.Update(entity);
+        }
+    }
+}
