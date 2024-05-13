2ab817ea0ad031cf8d8f75782187010531321687
OzanUlus <ilkerozanulus@gmail.com>
2024-05-13 21:50:51 +0300

    done mappings

---
+++ SignalRApi/Mapping/ProductMapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using SignalR.DtoLayer.ProductDto;
+
+namespace SignalRApi.Mapping
+{
+    public class ProductMapping : Profile
+    {
+        public ProductMapping()
+        {
+            CreateMap<ProductMapping , ResultProductDto>().ReverseMap();
+            CreateMap<ProductMapping , CreateProductDto>().ReverseMap();
+            CreateMap<ProductMapping , UpdateProductDto>().ReverseMap();
+            CreateMap<ProductMapping , ListProductDto>().ReverseMap();
+        }
+    }
+}
